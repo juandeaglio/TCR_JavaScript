e3c0b63dabc76722277474607cc678f24ad47082
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-11-09 17:02:18 -0500

    Fixed package.json commoand

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const MyComponent: React.FC = () => {
-  return <h1>Hello, World!</h1>;
+  return(
+    <h1>Hello, World!
+      <p>Not necessaryu</p>
+    </h1>
+  );
 };
 
 export default MyComponent;
