dd1627b000f1c1b70e3a1f871845ecfc08eb04cc
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-11-26 17:03:02 -0500

    Tests passed.

--- test/integration/BoxCollisionSystem.integration.test.tsx
+++ test/integration/BoxCollisionSystem.integration.test.tsx
@@ -4,20 +4,38 @@ import Box from '../../src/components/Box/Box'
 import '@testing-library/jest-dom';
 
 
-const TestApp = () => {
-    // creates an svg element that is size of screen with a box react functional component within
+
+enum Direction {
+    None = 0 % 5,
+    Up = 1 % 5,
+    Right = 2 % 5,
+    Down = 3 % 5,
+    Left = 4 % 5,
+}
+
+type TestAppProps = {
+    direction: Direction;
+};
+
+const TestApp: React.FC<TestAppProps> = ({ direction }) => {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
+    // Assuming Box expects an object with windowWidth and widthHeight
+    const boxProps = {
+        windowWidth: screenWidth,
+        widthHeight: screenHeight,
+    };
+
     return (
         <svg width={screenWidth} height={screenHeight}>
-            <Box windowWidth={screenWidth} widthHeight={screenHeight} />
+            {Box(boxProps)}
         </svg>
     );
 };
 
 test('A box exists in the center of the screen', () => {
-    render(<TestApp />);
+    render(<TestApp direction={0} />);
     const box = screen.getByRole('box');
     expect(box).toBeInTheDocument();
 
@@ -25,6 +43,19 @@ test('A box exists in the center of the screen', () => {
     const centerY = window.innerHeight / 2;
 
     expect(box).toHaveAttribute('x', centerX.toString());
+    expect(box).toHaveAttribute('y', centerY.toString());
+});
+
+test('A box moves from center to the right edge of the screen', () => {
+    // dependency injection of a box into TestApp, one with a move right velocity that reaches the right edge of the screen in 1 second    
+    render(<TestApp direction={'right'} />);
+    const box = screen.getByRole('box');
+    expect(box).toBeInTheDocument();
+
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    expect(box).toHaveAttribute('x', (centerX + 10).toString());
     expect(box).toHaveAttribute('y', centerY.toString());
 });
 
