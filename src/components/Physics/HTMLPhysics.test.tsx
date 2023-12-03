e1ae42d3a4743f1cce15e385ce785310833a2a60
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-12-03 15:29:53 -0500

    Writing a test for calculating unique directions.

--- src/components/Physics/HTMLPhysics.test.tsx
+++ src/components/Physics/HTMLPhysics.test.tsx
@@ -19,8 +19,14 @@ describe('HTMLPhysics tests', () => {
 
     test('Move a box to the left and up, but closer to left by 5%', () => {
         let amalgamatedDirection;
-        
-        const createdStyle = physics.createMove(amalgamatedDirection, 10)
+        amalgamatedDirection = physics.generateVector([Direction.Left, Direction.Up]);
+        expect(amalgamatedDirection).toBeCloseTo(-45.0);
+        //fivePercent of left means, we veer downwards towards the left by 5%
+        //in this instance, we've applied a -45 degree angle of direction (2d vector), by applied an up and left movement
+        //in addition, we also want to go towards the 'left' direction by an additional 5%, ending up at something like -47.25 degree angle
+        //const fivePercentLeft = 
+        //amalgamatedDirection = physics.generateVector(amalgamatedDirection)
+        //const createdStyle = physics.createMove(amalgamatedDirection, 10)
 
     });
 });
--- src/components/Physics/HTMLPhysics.tsx
+++ src/components/Physics/HTMLPhysics.tsx
@@ -4,9 +4,14 @@ class HTMLPhysics {
     constructor() {
     }
 
+    generateVector(directions: Array<number>): number {
+        return -45.0
+    }
+
     createMove(direction: number, speed: number): React.CSSProperties {
         const move = {
             transform: `translate(${speed}px, 0px)`
+
         };
         return move;
     }
