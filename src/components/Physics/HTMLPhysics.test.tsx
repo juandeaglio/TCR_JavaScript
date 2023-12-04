8c5a0b4c8b38cd45d69abdc580448e8454d9849c
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-12-04 13:28:06 -0500

    Defining new unit test for vector calculation of three vectors instead of two.

--- src/components/Physics/HTMLPhysics.test.tsx
+++ src/components/Physics/HTMLPhysics.test.tsx
@@ -19,9 +19,22 @@ describe('HTMLPhysics tests', () => {
 
     test('Move a box to the left and up', () => {
         let amalgamatedDirection;
-        amalgamatedDirection = physics.generateVectorFromVectors([new Vector(315.0, Math.sqrt(2)), new Vector(270.0, 1)])
+        const vector1 = new Vector(360.0, 1);
+        const vector2 = new Vector(270.0, 1);
+        amalgamatedDirection = physics.generateVectorFromVectors([vector1, vector2]);
+
         expect(amalgamatedDirection.direction).toBeCloseTo(315.0);
         expect(amalgamatedDirection.speed).toBeCloseTo(Math.sqrt(2));
+
+    });
+
+    test('Move a box with three vectors', () => {
+        let amalgamatedDirection;
+        const vector1 = new Vector(360.0, 1);
+        const vector2 = new Vector(270.0, 1);
+        const vector3 = new Vector(vector2.direction, vector2.speed * 0.05);
+        amalgamatedDirection = physics.generateVectorFromVectors([vector1, vector2]);
+        
         //fivePercentLeft means, we veer downwards towards the left by 5%
         //in this instance, we've applied a -45 degree angle of direction (2d vector), by applied an up and left movement
         //in addition, we also want to go towards the 'left' direction by an additional 5%, ending up at something like -47.25 degree angle
@@ -30,6 +43,5 @@ describe('HTMLPhysics tests', () => {
 
         //amalgamatedDirection = physics.generateVector(amalgamatedDirection)
         //const createdStyle = physics.createMove(amalgamatedDirection, 10)
-
     });
 });
