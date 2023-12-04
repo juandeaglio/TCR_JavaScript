7a8f81740a8d0689c45340a5825b984b11dc874b
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-12-04 13:23:04 -0500

    Tests passed.

--- src/components/Physics/HTMLPhysics.test.tsx
+++ src/components/Physics/HTMLPhysics.test.tsx
@@ -17,12 +17,12 @@ describe('HTMLPhysics tests', () => {
         expect(createdStyle).toEqual(actualStyle);
     });
 
-    test('Move a box to the left and up, but closer to left by 5%', () => {
+    test('Move a box to the left and up', () => {
         let amalgamatedDirection;
         amalgamatedDirection = physics.generateVectorFromVectors([new Vector(315.0, Math.sqrt(2)), new Vector(270.0, 1)])
         expect(amalgamatedDirection.direction).toBeCloseTo(315.0);
         expect(amalgamatedDirection.speed).toBeCloseTo(Math.sqrt(2));
-        //fivePercent of left means, we veer downwards towards the left by 5%
+        //fivePercentLeft means, we veer downwards towards the left by 5%
         //in this instance, we've applied a -45 degree angle of direction (2d vector), by applied an up and left movement
         //in addition, we also want to go towards the 'left' direction by an additional 5%, ending up at something like -47.25 degree angle
         // let fivePercentLeft = amalgamatedDirection - Direction.Left;
