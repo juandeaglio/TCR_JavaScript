93ce1117e3b5409d9f10438fb168be01a9fbce0e
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-12-04 16:58:38 -0500

    Wrote out the calculation for test case in a comment.

--- src/components/Physics/HTMLPhysics.test.tsx
+++ src/components/Physics/HTMLPhysics.test.tsx
@@ -31,17 +31,27 @@ describe('HTMLPhysics tests', () => {
     test('Move a box with three vectors', () => {
         let amalgamatedDirection;
         const vector1 = new Vector(360.0, 1);
-        const vector2 = new Vector(270.0, 1);
-        const vector3 = new Vector(vector2.direction, vector2.speed * 0.05);
+        const vector2 = new Vector(-90.0, 1);
+        const vector3 = new Vector(Direction.Down, vector2.speed * 0.05); // five percent downwards
         amalgamatedDirection = physics.generateVectorFromVectors([vector1, vector2]);
-        
-        //fivePercentLeft means, we veer downwards towards the left by 5%
-        //in this instance, we've applied a -45 degree angle of direction (2d vector), by applied an up and left movement
-        //in addition, we also want to go towards the 'left' direction by an additional 5%, ending up at something like -47.25 degree angle
-        // let fivePercentLeft = amalgamatedDirection - Direction.Left;
-        // fivePercentLeft -= fivePercentLeft * 0.05;
 
-        //amalgamatedDirection = physics.generateVector(amalgamatedDirection)
-        //const createdStyle = physics.createMove(amalgamatedDirection, 10)
+        // The calculation should go like this:
+        // V1 = new Vector(360.0, 1);
+        // V2 = new Vector(-90.0, 1);
+        // V3 = new Vector(180.0, 1 * 0.05); // five percent downwards
+        //
+        // Ax = V1.speed * cos(Theta-A) = 1 * cos(360.0) = 1
+        // Ay = V1.speed * sin(Theta-A) = 1 * sin(360.0) = 0
+        // Bx = V2.speed * cos(Theta-B) = 1 * cos(-90.0) = 0
+        // By = V2.speed * sin(Theta-B) = 1 * sin(-90.0) = -1
+        // Cx = V3.speed * cos(Theta-C) = 0.05 * cos(180.0) = -0.05
+        // Cy = V3.speed * sin(Theta-C) = 0.05 * sin(180.0) = 0
+
+        // Dx = Ax + Bx + Cx = 1 + 0 - 0.05 = 0.95
+        // Dy = Ay + By + Cy = 0 + (-1) + 0 = -1
+
+        // Magnitude = sqrt(Dx^2 + Dy^2) = sqrt((0.95)^2 + (-1)^2) = sqrt(0.9025 + 1) = sqrt(1.9025) = 1.37931142241
+        // Theta = arctan(Dy / Dx) = arctan(-1 / 0.95) = arctan(-1.05263157895) = -46.4688007 degrees = 0.759762755 radians
+        // amalgamatedDirection = new Vector(-46.4688007, 1.37931142241);
     });
 });
