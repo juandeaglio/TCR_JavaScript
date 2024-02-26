811e7b0ffa452aba28fbfb392e8f65ffd13acadb
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-26 14:02:19 -0500

    getAllMoveables and Paused methods

--- src/systems/TestSimulation.test.tsx
+++ src/systems/TestSimulation.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { unpause, pause } from './TestSimulation';
+import { unpause, pause, getAllMoveables, getAllPaused} from './TestSimulation';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
@@ -11,11 +11,11 @@ test('unpauses multiple moveable nodes', () => {
             <a className='moveable pause-animation' data-test-id="a-2"></a>
         </div>
         );
-    let elements: HTMLCollectionOf<Element> = container.getElementsByClassName('pause-animation');
-    expect(elements.length).toBe(2)
+    let elements: HTMLCollectionOf<Element> = getAllPaused(container);
+    expect(elements.length).toBe(2);
     unpause(elements);
-    elements = container.getElementsByClassName('pause-animation');
-    expect(elements.length).toBe(0)
+    elements = getAllPaused(container);
+    expect(elements.length).toBe(0);
 });
 
 test('pauses multiple moveable nodes', () => {
@@ -26,10 +26,41 @@ test('pauses multiple moveable nodes', () => {
             <a className='moveable' data-test-id="a-2"></a>
         </div>
         );
-    let elements: HTMLCollectionOf<Element> = container.getElementsByClassName('moveable');
-    let pausedElements: HTMLCollectionOf<Element> = container.getElementsByClassName('pause-animation');
+    let elements: HTMLCollectionOf<Element> = getAllMoveables(container);
+    let pausedElements: HTMLCollectionOf<Element> = getAllPaused(container);
     expect(pausedElements.length).toBe(0)
     pause(elements);
-    elements = container.getElementsByClassName('pause-animation');
+    elements = getAllPaused(container);
+    expect(elements.length).toBe(2)
+});
+
+test('unpauses all moveable nodes', () => {
+    const { container } = render(
+        <div>
+            <a className='moveable pause-animation' data-test-id="a-1"></a>
+            <a> Hello World </a>
+            <a className='moveable pause-animation' data-test-id="a-2"></a>
+        </div>
+        );
+    let elements: HTMLCollectionOf<Element> = getAllMoveables(container);
+    expect(elements.length).toBe(2)
+    unpause(elements);
+    elements = getAllPaused(container);
+    expect(elements.length).toBe(0)
+});
+
+test('pauses all moveable nodes', () => {
+    const { container } = render(
+        <div>
+            <a className='moveable' data-test-id="a-1"></a>
+            <a> Hello World </a>
+            <a className='moveable' data-test-id="a-2"></a>
+        </div>
+        );
+    let elements: HTMLCollectionOf<Element> = getAllMoveables(container);
+    let pausedElements: HTMLCollectionOf<Element> = getAllPaused(container);
+    expect(pausedElements.length).toBe(0)
+    pause(elements);
+    elements = getAllPaused(container);
     expect(elements.length).toBe(2)
 });
--- src/systems/TestSimulation.tsx
+++ src/systems/TestSimulation.tsx
@@ -14,3 +14,13 @@ export function pause(elements: HTMLCollectionOf<Element>): void
         element.classList.add("pause-animation");
     });
 }
+
+export function getAllPaused(document: HTMLElement): HTMLCollectionOf<Element>
+{
+    return document.getElementsByClassName("pause-animation");
+}
+
+export function getAllMoveables(document: HTMLElement): HTMLCollectionOf<Element>
+{
+    return document.getElementsByClassName("moveable");
+}
